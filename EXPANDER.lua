bb12aebb9f50705b5b742d841825b9533ee45e76
AFKZxc <165298844+AFKZxc@users.noreply.github.com>
2024-07-17 11:22:38 +0800

    Update EXPANDER.lua

--- EXPANDER.lua
+++ EXPANDER.lua
@@ -211,8 +211,8 @@ getgenv().filluseteamcolor = true --Toggle fill color using player team color on
 getgenv().outlineuseteamcolor = true --Toggle outline color using player team color on/off
 getgenv().fillcolor = Color3.new(0, 0, 0) --Change fill color, no need to edit if using team color
 getgenv().outlinecolor = Color3.new(1, 1, 1) --Change outline color, no need to edit if using team color
-getgenv().filltrans = 0.5 --Change fill transparency
-getgenv().outlinetrans = 0.5 --Change outline transparency
+getgenv().filltrans = 0.6 --Change fill transparency
+getgenv().outlinetrans = 0.1 --Change outline transparency
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/RobloxScripts/main/Highlight-ESP.lua"))()
 end)
